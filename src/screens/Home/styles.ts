e8f5a252df9aa65486e7d7ba70e78a74a680cbbd
Valenttin Motter <valenttin@myscrumhalf.com>
2024-09-06 20:11:33 -0300

    feat: criando componente header

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  headerContainer: {
+    height: 173,
+    backgroundColor: "#7A4A9E",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "700",
+    color: "#F2F2F2",
+  },
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "#F2F2F2",
+  },
+});
