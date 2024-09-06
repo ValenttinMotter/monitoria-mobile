d452a0c2ff3d1d9eba0a417b0b437d724bdd5481
Valenttin Motter <valenttin@myscrumhalf.com>
2024-09-06 20:53:34 -0300

    feat: criação do input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 export function Home() {
   return (
@@ -7,7 +8,18 @@ export function Home() {
       <View style={styles.headerContainer}>
         <Text style={styles.title}>Lista de Compras</Text>
       </View>
-      <View style={styles.mainContainer}></View>
+      <View style={styles.mainContainer}>
+        <View style={styles.inputWrapper}>
+          <TextInput
+            style={styles.input}
+            placeholder="Adicione um novo produto"
+            placeholderTextColor={"#808080"}
+          ></TextInput>
+          <TouchableOpacity style={styles.button}>
+            <AntDesign name="pluscircleo" size={18} color="#fff" />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
